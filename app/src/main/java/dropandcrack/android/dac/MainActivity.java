fc7532fc4ea4eec0a858c16a2e3d27892c0a1428
Younggyun Jung <yg.jung@tridge.com>
2015-11-01 01:34:55 +0900

    fixed untracked files

--- app/src/main/java/dropandcrack/android/dac/MainActivity.java
+++ app/src/main/java/dropandcrack/android/dac/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 
 public class MainActivity extends Activity {
 
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
